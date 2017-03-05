a186fadda83bdb5273d2cd6dd12d6a7ba07ad147
Joachim Metz <joachim.metz@gmail.com>
2017-03-05 18:08:35 +0100

    Worked on dependencies

--- config/travis/install.sh
+++ config/travis/install.sh
@@ -4,9 +4,9 @@
 
 COVERALL_DEPENDENCIES="python-coverage python-coveralls python-docopt";
 
-PYTHON2_DEPENDENCIES="libbde-python libewf-python libexe-python libfsntfs-python libfvde-python libfwnt-python libfwsi-python libqcow-python libregf-python libsigscan-python libsmdev-python libsmraw-python libvhdi-python libvmdk-python libvshadow-python libvslvm-python libwrc-python python-construct python-crypto python-dfdatetime python-dfvfs python-dfwinreg python-pytsk3 python-six";
+PYTHON2_DEPENDENCIES="libbde-python libewf-python libexe-python libfsntfs-python libfvde-python libfwnt-python libqcow-python libregf-python libsigscan-python libsmdev-python libsmraw-python libvhdi-python libvmdk-python libvshadow-python libvslvm-python libwrc-python python-construct python-crypto python-dfdatetime python-dfvfs python-dfwinreg python-pytsk3 python-six";
 
-PYTHON3_DEPENDENCIES="libbde-python3 libewf-python3 libexe-python3 libfsntfs-python3 libfvde-python3 libfwnt-python3 libfwsi-python3 libqcow-python3 libregf-python3 libsigscan-python3 libsmdev-python3 libsmraw-python3 libvhdi-python3 libvmdk-python3 libvshadow-python3 libvslvm-python3 libwrc-python3 python3-construct python3-crypto python3-dfdatetime python3-dfvfs python3-dfwinreg python3-pytsk3 python3-six";
+PYTHON3_DEPENDENCIES="libbde-python3 libewf-python3 libexe-python3 libfsntfs-python3 libfvde-python3 libfwnt-python3 libqcow-python3 libregf-python3 libsigscan-python3 libsmdev-python3 libsmraw-python3 libvhdi-python3 libvmdk-python3 libvshadow-python3 libvslvm-python3 libwrc-python3 python3-construct python3-crypto python3-dfdatetime python3-dfvfs python3-dfwinreg python3-pytsk3 python3-six";
 
 
 # Exit on error.
